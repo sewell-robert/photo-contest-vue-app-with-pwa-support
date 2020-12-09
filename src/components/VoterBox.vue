43fc2240cbdef1b75e515bfea4bec30e0c15c4be
robertcsewelljr@gmail.com <robertcsewelljr@gmail.com>
2020-12-08 19:45:06 -0600

    Refactoring in VoterBox component to increase performance

--- src/components/VoterBox.vue
+++ src/components/VoterBox.vue
@@ -20,55 +20,29 @@
               :retain-focus="false"
             >
               <template v-slot:activator="{ on, attrs }">
-                <div>
-                  <div v-if="result.imgUrlLowQuality">
-                    <v-img
-                      :src="result.imgUrlLowQuality"
-                      aspect-ratio="1"
-                      class="grey lighten-2"
-                      v-bind="attrs"
-                      v-on="on"
-                      v-on:click="cacheIndexForDialog(index)"
+                <v-img
+                  :src="result.imgUrlLowQuality"
+                  aspect-ratio="1"
+                  class="grey lighten-2"
+                  v-bind="attrs"
+                  v-on="on"
+                  v-on:click="cacheIndexForDialog(index)"
+                >
+                  <template v-slot:placeholder>
+                    <v-row
+                      class="fill-height ma-0"
+                      align="center"
+                      justify="center"
                     >
-                      <template v-slot:placeholder>
-                        <v-row
-                          class="fill-height ma-0"
-                          align="center"
-                          justify="center"
-                        >
-                          <v-progress-circular
-                            indeterminate
-                            color="grey lighten-5"
-                          ></v-progress-circular>
-                        </v-row>
-                      </template>
-                    </v-img>
-                  </div>
-                  <div v-else>
-                    <v-img
-                      :src="result.imgUrl"
-                      aspect-ratio="1"
-                      class="grey lighten-2"
-                      v-bind="attrs"
-                      v-on="on"
-                      v-on:click="cacheIndexForDialog(index)"
-                    >
-                      <template v-slot:placeholder>
-                        <v-row
-                          class="fill-height ma-0"
-                          align="center"
-                          justify="center"
-                        >
-                          <v-progress-circular
-                            indeterminate
-                            color="grey lighten-5"
-                          ></v-progress-circular>
-                        </v-row>
-                      </template>
-                    </v-img>
-                  </div>
-                </div>
+                      <v-progress-circular
+                        indeterminate
+                        color="grey lighten-5"
+                      ></v-progress-circular>
+                    </v-row>
+                  </template>
+                </v-img>
               </template>
+
               <v-card>
                 <v-img
                   :src="photoUrlDialog"
@@ -179,7 +153,7 @@ export default {
     nextPage () {
       UploadService.getNextPage(this.page)
         .then((response) => {
-          this.results = response.data
+          this.results = response.data.photoDataList
         })
     },
     cacheIndexForDialog (index) {
@@ -237,16 +211,31 @@ export default {
   created () {
     UploadService.getNextPage(1)
       .then((response) => {
-        this.results = response.data
-      }).then(UploadService.getPhotoCount()
-        .then((response) => {
-          this.paginationLength = Math.ceil(response.data / 6)
-          console.log(response.data)
+        console.log(response.data)
+        console.log(response.data.photoCount)
+        console.log(response.data.photoDataList)
 
-          if (response.data === 0) {
-            this.isResultsEmpty = true
-          }
-        }))
+        this.results = response.data.photoDataList
+
+        this.paginationLength = Math.ceil(response.data.photoCount / 6)
+
+        if (response.data.photoCount === 0) {
+          this.isResultsEmpty = true
+        }
+      })
+
+    // UploadService.getNextPage(1)
+    //   .then((response) => {
+    //     this.results = response.data
+    //   }).then(UploadService.getPhotoCount()
+    //     .then((response) => {
+    //       this.paginationLength = Math.ceil(response.data / 6)
+    //       console.log(response.data)
+
+    //       if (response.data === 0) {
+    //         this.isResultsEmpty = true
+    //       }
+    //     }))
 
     var newDate = new Date()
     var todaysDt = newDate.getDate()
